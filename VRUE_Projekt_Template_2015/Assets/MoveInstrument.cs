8a5af93bf54250065fe7c4dd523bf4d56b814a42
phil <e1027173@student.tuwien.ac.at>
2015-11-28 01:45:09 +0100

    assets for instruments

---
+++ VRUE_Projekt_Template_2015/Assets/MoveInstrument.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInstrument : MonoBehaviour {
+
+    public GameObject instrument;
+
+    GameObject tracker;
+
+    
+
+    Transform oldPosition;
+	// Use this for initialization
+	void Start () {
+        tracker = GameObject.Find("TrackerObject");
+	    
+        if(tracker && instrument)
+        {
+            Debug.Log("Button pressed");
+
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    /*
+    [SerializeField]
+    public Button MyButton = null; // assign in the editor
+
+    void Start()
+    {
+        MyButton.onClick.AddListener(() => { MyFunction(); MyOtherFunction(); });
+    }
+     * 
+     */
+}
