e5838a28f0fd282558fcad1097c0d1cf452dca36
phil <e1027173@student.tuwien.ac.at>
2015-11-28 13:11:33 +0100

    button for testinstrument, activating spacemouse and movement

--- VRUE_Projekt_Template_2015/Assets/MoveInstrument.cs
+++ VRUE_Projekt_Template_2015/Assets/MoveInstrument.cs
@@ -9,15 +9,19 @@ public class MoveInstrument : MonoBehaviour {
     GameObject categoryParent;
     Transform initialTransform;
     GameObject tracker;
-    
+    GameObject spaceMouse;
 
     Transform oldPosition;
 
     Button button;
 	// Use this for initialization
 	void Start () {
-        tracker = GameObject.Find("SpaceMouse").transform.Find("TrackerObject").gameObject;
-	    
+        //@lva: objects do not seem to have a fixed naming convention :P
+        //either use camelcase everywhere or don't
+        tracker = GameObject.Find("TrackingCamera");
+        spaceMouse = tracker.transform.Find("Spacemouse").gameObject;
+        tracker = spaceMouse.transform.Find("TrackerObject").gameObject;
+        Debug.Log(tracker);
         if(tracker && instrument)
         {
             Debug.Log("Button pressed");
@@ -41,7 +45,7 @@ public class MoveInstrument : MonoBehaviour {
     void OnClick()
     {
         Debug.Log("Button clicked");
-
+        spaceMouse.active = true;
         MoveInstrumentToSpaceMouseParent();
     }
     
