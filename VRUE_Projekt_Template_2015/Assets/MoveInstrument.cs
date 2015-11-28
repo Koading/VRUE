d8bcbf000d1b6f2eafc5dd7dea8a7baf0e6dfa10
phil <e1027173@student.tuwien.ac.at>
2015-11-28 12:14:32 +0100

    template script to move object to spacemouse hierarchy

--- VRUE_Projekt_Template_2015/Assets/MoveInstrument.cs
+++ VRUE_Projekt_Template_2015/Assets/MoveInstrument.cs
@@ -1,39 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MoveInstrument : MonoBehaviour {
 
     public GameObject instrument;
 
+    GameObject categoryParent;
+    Transform initialTransform;
     GameObject tracker;
-
     
 
     Transform oldPosition;
+
+    Button button;
 	// Use this for initialization
 	void Start () {
-        tracker = GameObject.Find("TrackerObject");
+        tracker = GameObject.Find("SpaceMouse").transform.Find("TrackerObject").gameObject;
 	    
         if(tracker && instrument)
         {
             Debug.Log("Button pressed");
 
-
+            categoryParent = instrument.transform.parent.gameObject;
+            initialTransform = instrument.transform;
+            button = this.GetComponent<Button>();
+            button.onClick.AddListener(
+                    this.OnClick
+                );
         }
+
     }
 	
 	// Update is called once per frame
 	void Update () {
 	    
 	}
-    /*
-    [SerializeField]
-    public Button MyButton = null; // assign in the editor
 
-    void Start()
+    
+    void OnClick()
     {
-        MyButton.onClick.AddListener(() => { MyFunction(); MyOtherFunction(); });
+        Debug.Log("Button clicked");
+
+        MoveInstrumentToSpaceMouseParent();
     }
-     * 
-     */
+    
+    void MoveInstrumentToSpaceMouseParent()
+    {
+        if(instrument)
+        {
+            instrument.transform.parent = tracker.transform;
+        }
+    }
+
+    void ResetInstrument()
+    {
+        if(instrument)
+        {
+            instrument.transform.localPosition = initialTransform.localPosition;
+            instrument.transform.eulerAngles = initialTransform.eulerAngles;
+
+            instrument.transform.parent = categoryParent.transform;
+        }
+    }
+
 }
