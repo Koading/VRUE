6720aa78a51cb763535ca91a091032779f73b44d
phil <e1027173@student.tuwien.ac.at>
2015-12-01 14:10:20 +0100

    tl;dr volume settings with spacemouse

--- VRUE_Projekt_Template_2015/Assets/src/InstrumentBehaviour.cs
+++ VRUE_Projekt_Template_2015/Assets/src/InstrumentBehaviour.cs
@@ -8,25 +8,50 @@ public class InstrumentBehaviour : MonoBehaviour {
     //public AudioClip clip;
 
     public AudioSource audioSource;
-	// Use this for initialization
+
+    private Animator animation;
+    // Use this for initialization
 
     public bool playState = false;
 	private bool alreadyChangedState = false;
 	private bool selected = false;
 	private float maxVolumne;
 
+    public bool isSelected;
 	public float incrementalDecline = 0.1f;
 
+    GameObject spaceMouse;
+
+
+    private float oldPostionY;
 	void Start () {
         Debug.Log("InstrumentBehaviour Start");
 
 		audioSource = this.gameObject.GetComponent<AudioSource>();
+        animation = this.GetComponent<Animator>();
 		maxVolumne = audioSource.volume;
 
+        spaceMouse = GameObject.Find("Spacemouse");
+        oldPostionY = spaceMouse.transform.localEulerAngles.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(isSelected && audioSource)
+        {
+            float deltaSpaceY = spaceMouse.transform.localEulerAngles.y;
+            
+            //Debug.Log("deltay: " + deltaSpaceY);
+            
+            if (spaceMouse.transform.localEulerAngles.y < 10f)
+                deltaSpaceY = 0;
+            if (spaceMouse.transform.localEulerAngles.y >= 180f)
+                deltaSpaceY = spaceMouse.transform.localEulerAngles.y - 360f;
+
+            maxVolumne = Mathf.Max(Mathf.Min( maxVolumne + maxVolumne * (-deltaSpaceY / 360f) * Time.deltaTime, 1.0f),0.2f);
+        }
+
 	    //TODO kinect stuff
         Debug.Log("InstrumentBehaviour Update");
         if (selected && !alreadyChangedState) {
@@ -39,27 +64,32 @@ public class InstrumentBehaviour : MonoBehaviour {
 			}
 		}
 
-		if (playState && audio) { 
+		if (playState && audioSource) { 
             
 			if(audioSource.volume > 0.0f) {
 				audioSource.volume -= Mathf.Max(0.0f, incrementalDecline * Time.deltaTime);
 			}
 			Debug.Log("Music is now on volume: " + audioSource.volume);
 
+            //Debug.Log("KeyPressed E:" + Input.GetKeyDown(KeyCode.E));
 			if(Input.GetKeyDown(KeyCode.E)) {
+                
 				if (!audioSource.loop) { 
 					audioSource.loop = true;
 					audioSource.Play ();
+                    animation.enabled = true;
 				} 
 
 				if(audioSource.volume < 0.1f) {
 					audioSource.Stop();
 					audioSource.Play();
+                    animation.enabled = false;
 				}
 
 				audioSource.volume = maxVolumne;
 			}			
 		}
+        //oldPostionY = spaceMouse.transform.localEulerAngles.y;
 	}
 
 
