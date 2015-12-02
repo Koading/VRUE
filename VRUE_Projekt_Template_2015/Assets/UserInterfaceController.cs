eb12b231a0abb0fde5708ae8c033c6569afb8357
phil <e1027173@student.tuwien.ac.at>
2015-12-02 20:27:59 +0100

    models for additional points: audience; A controller script that activates user interface if a client is connected (as spacemouseUser)

---
+++ VRUE_Projekt_Template_2015/Assets/UserInterfaceController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UserInterfaceController : MonoBehaviour {
+
+    GameObject userInterface;
+
+	// Use this for initialization
+	void Start () {
+        userInterface = GameObject.Find("UserInterface");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnConnect();
+	}
+
+    void OnConnect()
+    {
+        if (Network.isClient && !userInterface.transform.Find("Canvas SpaceMouseUser").active)
+        {
+            userInterface.transform.Find("Canvas SpaceMouseUser").active = true;
+        }
+    }
+}
