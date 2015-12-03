fef2125ca312c9d41f20d98b4109d58869ba4dd7
phil <e1027173@student.tuwien.ac.at>
2015-12-03 19:24:38 +0100

    audiencebehaviour, animation

--- VRUE_Projekt_Template_2015/Assets/AudienceBehaviour.cs
+++ VRUE_Projekt_Template_2015/Assets/AudienceBehaviour.cs
@@ -4,12 +4,29 @@ using System.Collections;
 public class AudienceBehaviour : MonoBehaviour {
 
 	// Use this for initialization
+
+    const string instrumentPool = "Active Instrument Pool";
+    
+    private GameObject assignedInstrument = null;
+    private InstrumentBehaviour assignedInstrumentScript = null;
+    private Animation animation;
 	void Start () {
-	
+        animation = this.transform.parent.GetComponent<Animation>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(assignedInstrumentScript)
+        {
+            //if(assignedInstrumentScript.audioSource.active< )
+            {
+
+            }
+        }
 	}
+
+    public void assignInstrument(InstrumentBehaviour ib)
+    {
+        assignedInstrumentScript = ib;
+    }
 }
